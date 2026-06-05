addb33748081d106819ea2eeffd5049d6d118143
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:53 -0500

    xml commenting

--- src/Soenneker.Together.OpenApiClientUtil/Abstract/ITogetherOpenApiClientUtil.cs
+++ src/Soenneker.Together.OpenApiClientUtil/Abstract/ITogetherOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Together.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ITogetherOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<TogetherOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
